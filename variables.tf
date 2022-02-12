9007d9764706703372c30b73f5de1f7bb1d8018c
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2022-02-12 14:10:43 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "inst_ami" {
+  type = "string"
+}
+
+variable "inst_type" {
+  type = "string"
+}
+
+variable "inst_key" {
+  type = "string"
+}
+
+variable "tags" {
+  type        = "map"
+}
