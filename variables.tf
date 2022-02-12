b08e58bd5fd08acd08a01655be742a363631b295
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2022-02-12 14:19:44 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,11 @@
 variable "inst_ami" {
-  type = "string"
 }
 
 variable "inst_type" {
-  type = "string"
 }
 
 variable "inst_key" {
-  type = "string"
 }
 
 variable "tags" {
-  type        = "map"
 }
